dbae5f4c28403aa743fb100f0c67a94219aa6b29
Josi Gama <josyvisa@gmail.com>
2016-04-03 19:50:12 -0300

    @josigama - Add database config and include dependecy for liquibase-core

--- src/main/java/com/example/DatabaseConfig.java
+++ src/main/java/com/example/DatabaseConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
-
 @Configuration
 public class DatabaseConfig {
     @Bean
